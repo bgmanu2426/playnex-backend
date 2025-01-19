02dd092e8734c89e4e1bb027279bb84e8132ef64
manubg <bgmanu2426@gmail.com>
2025-01-19 20:28:21 +0530

    feat: add healthcheck route and related controller, update app routing

--- src/app.js
+++ src/app.js
@@ -31,10 +31,24 @@ app.use(express.static("public")); // use express.static() middleware to serve s
 app.use(cookieParser()); // use cookieParser() middleware to parse cookies
 
 // Routes
-import userRoutes from "./routes/user.routes.js";
+import userRoutes from "./routes/user.route.js";
+import videoRoutes from "./routes/video.route.js";
+import commentRoutes from "./routes/comment.route.js";
+import dashboardRoutes from "./routes/dashboard.route.js";
+import playlistRoutes from "./routes/playlist.route.js";
+import healthCheckRoutes from "./routes/healthcheck.route.js";
+import subscriptionRoutes from "./routes/subscription.route.js";
+import tweetRoutes from "./routes/tweet.route.js";
 
 // Routes declaration
 app.use("/api/v1/users", userRoutes); // use user routes
+app.use("/api/v1/videos", videoRoutes); // use video routes
+app.use("/api/v1/comments", commentRoutes); // use comment routes
+app.use("/api/v1/dashboard", dashboardRoutes); // use dashboard routes
+app.use("/api/v1/playlist", playlistRoutes); // use playlist routes
+app.use("/api/v1/health", healthCheckRoutes); // use healthcheck routes
+app.use("/api/v1/subscriptions", subscriptionRoutes); // use subscription routes
+app.use("/api/v1/tweets", tweetRoutes); // use tweet routes
 
 // Global Error Handling Middleware
 import errorMiddleware from "./middlewares/error.middleware.js";
