b5119128262132dfe8a45e0a5db800c391a91597
manubg <bgmanu2426@gmail.com>
2025-01-25 10:07:17 +0530

    feat: enhance like management; implement toggling likes for videos, comments, and tweets, and add endpoint to retrieve liked videos

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,26 +1,153 @@
-import mongoose, { isValidObjectId } from "mongoose";
+import { isValidObjectId } from "mongoose";
 import { Like } from "../models/like.model.js";
+import { Video } from "../models/video.model.js";
+import { Comment } from "../models/comment.model.js";
+import { Tweet } from "../models/tweet.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 
+/**
+ * @desc Toggle like on a video
+ * @route POST /api/v1/likes/toggle/v/:videoId
+ * @access Private
+ */
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const { videoId } = req.params;
-    //TODO: toggle like on video
+    try {
+        const { videoId } = req.params;
+        if (!isValidObjectId(videoId)) {
+            throw new ApiError(400, "Invalid Video ID");
+        }
+
+        const video = await Video.findById(videoId);
+        if (!video) {
+            throw new ApiError(404, "Video not found");
+        }
+
+        let userLikes = await Like.findOne({ likedBy: req.user._id });
+        if (!userLikes) {
+            userLikes = await Like.create({ likedBy: req.user._id });
+        }
+
+        const index = userLikes.videos.indexOf(videoId);
+        if (index >= 0) {
+            userLikes.videos.splice(index, 1);
+            await userLikes.save();
+            return res.status(200).json(new ApiResponse(200, "Video unliked"));
+        }
+
+        userLikes.videos.push(videoId);
+        await userLikes.save();
+        res.status(201).json(new ApiResponse(201, "Video liked", userLikes));
+    } catch (error) {
+        throw new ApiError(
+            error?.statusCode || 500,
+            error?.message || "Internal Server Error"
+        );
+    }
 });
 
+/**
+ * @desc Toggle like on a comment
+ * @route POST /api/v1/likes/toggle/c/:commentId
+ * @access Private
+ */
 const toggleCommentLike = asyncHandler(async (req, res) => {
-    const { commentId } = req.params;
-    //TODO: toggle like on comment
+    try {
+        const { commentId } = req.params;
+        if (!isValidObjectId(commentId)) {
+            throw new ApiError(400, "Invalid Comment ID");
+        }
+
+        const comment = await Comment.findById(commentId);
+        if (!comment) {
+            throw new ApiError(404, "Comment not found");
+        }
+
+        let userLikes = await Like.findOne({ likedBy: req.user._id });
+        if (!userLikes) {
+            userLikes = await Like.create({ likedBy: req.user._id });
+        }
+
+        const index = userLikes.comments.indexOf(commentId);
+        if (index >= 0) {
+            userLikes.comments.splice(index, 1);
+            await userLikes.save();
+            return res.status(200).json(new ApiResponse(200, "Comment unliked"));
+        }
+
+        userLikes.comments.push(commentId);
+        await userLikes.save();
+        res.status(201).json(new ApiResponse(201, "Comment liked", userLikes));
+    } catch (error) {
+        throw new ApiError(
+            error?.statusCode || 500,
+            error?.message || "Internal Server Error"
+        );
+    }
 });
 
+/**
+ * @desc Toggle like on a tweet
+ * @route POST /api/v1/likes/toggle/t/:tweetId
+ * @access Private
+ */
 const toggleTweetLike = asyncHandler(async (req, res) => {
-    const { tweetId } = req.params;
-    //TODO: toggle like on tweet
+    try {
+        const { tweetId } = req.params;
+        if (!isValidObjectId(tweetId)) {
+            throw new ApiError(400, "Invalid Tweet ID");
+        }
+
+        const tweet = await Tweet.findById(tweetId);
+        if (!tweet) {
+            throw new ApiError(404, "Tweet not found");
+        }
+
+        let userLikes = await Like.findOne({ likedBy: req.user._id });
+        if (!userLikes) {
+            userLikes = await Like.create({ likedBy: req.user._id });
+        }
+
+        const index = userLikes.tweets.indexOf(tweetId);
+        if (index >= 0) {
+            userLikes.tweets.splice(index, 1);
+            await userLikes.save();
+            return res.status(200).json(new ApiResponse(200, "Tweet unliked"));
+        }
+
+        userLikes.tweets.push(tweetId);
+        await userLikes.save();
+        res.status(201).json(new ApiResponse(201, "Tweet liked", userLikes));
+    } catch (error) {
+        throw new ApiError(
+            error?.statusCode || 500,
+            error?.message || "Internal Server Error"
+        );
+    }
 });
 
+/**
+ * @desc Get all liked videos by the current user
+ * @route GET /api/v1/likes/videos
+ * @access Private
+ */
 const getLikedVideos = asyncHandler(async (req, res) => {
-    //TODO: get all liked videos
+    try {
+        const userLikes = await Like.findOne({ likedBy: req.user._id })
+            .populate("videos");
+
+        // Return an empty array if the user has no like document
+        const likedVideos = userLikes?.videos || [];
+        return res
+            .status(200)
+            .json(new ApiResponse(200, "Liked videos fetched", likedVideos));
+    } catch (error) {
+        throw new ApiError(
+            error?.statusCode || 500,
+            error?.message || "Internal Server Error"
+        );
+    }
 });
 
-export { toggleCommentLike, toggleTweetLike, toggleVideoLike, getLikedVideos };
+export { toggleVideoLike, toggleCommentLike, toggleTweetLike, getLikedVideos };
