3e424b26ea0f6b10cf6e8d8a254e5ae1e7471892
manubg <bgmanu2426@gmail.com>
2025-01-28 22:46:25 +0530

    refactor: improve documentation for JWT verification middleware; enhance parameter descriptions and return type

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -5,11 +5,11 @@ import ApiError from "../utils/ApiError.js";
 import DATA from "../config.js";
 
 /**
- * Middleware to verify JWT token
- * @param {Object} req - The request object
- * @param {Object} res - The response object
- * @param {Function} next - The next middleware function
- * @throws {ApiError} - If the token is invalid or user is not found
+ * @desc - Middleware to verify the JWT token sent in the request.
+ * @param {Request} req - Express Request object.
+ * @param {Response} res - Express Response object.
+ * @param {NextFunction} next - Express Next function.
+ * @returns {void}
  */
 export const verifyJWT = asyncHandler(async (req, _, next) => {
     try {
